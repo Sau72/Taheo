ba68e00c5379213523ff4c2edcfc288e06a89f99
Ivanishchev Alexander <ua9lif@mail.ru>
2025-04-23 00:26:52 +0500

    Work 2 req

--- Taheo/src/main.cpp
+++ Taheo/src/main.cpp
@@ -1,86 +1,80 @@
-#include <Arduino.h>
 #include "BluetoothSerial.h"
 
-// Check if Bluetooth is properly enabled
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to enable it
 #endif
 
 BluetoothSerial SerialBT;
 
-// Delimiters and patterns
-const char START_CHAR = 'Z';
-const char END_CHAR = '\x03'; // ETX character (␃)
-const String REQUEST_PATTERN1 = "32091";
-// coarse
-const String RESPONSE1 = "?+00003157m2675319+0320026d+00003156*00+00+00062\x03"; //\x0D\x0A";  // Response with ETX
-// fine
-const String REQUEST_PATTERN2 = "34093";
+// coarse  Z32091
+//const String RESPONSE1 = "?+00003157m2675319+0320026d+00003156*00+00+00062\x03"; //\x0D\x0A";  // Response with ETX
+// fine 34093
 //const String RESPONSE2 = "(+000021760m26822425+34706355d+000021751t00+0000+000**070\x03"; //\x0D\x0A";  // Response with ETX
-const String RESPONSE2 = "(+000165690m09022425+22930134d+000021751t00+0000+000**070\x03"; //\x0D\x0A";  // Response with ETX
 
 
-void setup()
-{
-    Serial.begin(115200);
+// Variables to store the received values
+String X = "0320026";
+String Y = "2675319";
+String L = "00003157";
 
-    // Start Bluetooth Serial with a device name
-    SerialBT.begin("Taheo");
-
-    Serial.println("Bluetooth device started, waiting for connections...");
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("Taheo"); // Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
-void processRequest(String request)
-{
-    if (request == REQUEST_PATTERN1)
-    {
-        Serial.print("Received valid request: ");
-        Serial.println(request);
-
-        SerialBT.println(RESPONSE1);
-        Serial.print("Sent response: ");
-        Serial.println(RESPONSE1);
+void loop() {
+  // Check for incoming serial data (X,Y,L format)
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+    
+    // Parse the input in "X,Y,L" format
+    int firstComma = input.indexOf(',');
+    int secondComma = input.indexOf(',', firstComma + 1);
+    
+    if (firstComma != -1 && secondComma != -1) {
+      X = input.substring(0, firstComma);
+      Y = input.substring(firstComma + 1, secondComma);
+      L = input.substring(secondComma + 1);
+      
+      Serial.print("Received values - X: ");
+      Serial.print(X);
+      Serial.print(", Y: ");
+      Serial.print(Y);
+      Serial.print(", L: ");
+      Serial.println(L);
     }
-    else if (request == REQUEST_PATTERN2)
-    {
-        Serial.print("Received valid request: ");
-        Serial.println(request);
-
-        SerialBT.println(RESPONSE2);
-        Serial.print("Sent response: ");
-        Serial.println(RESPONSE2);
+  }
+  
+  // Check for incoming Bluetooth data
+  if (SerialBT.available()) {
+    String btInput = SerialBT.readStringUntil('\x03'); // ETX character
+    btInput.trim();
+    Serial.print("Received Bluetooth command: ");
+    Serial.println(btInput);
+    
+    if (btInput == "C067") {
+      SerialBT.println("\x06 006\x03");
+      Serial.println("Sent response: ACK 006␃");
+    } 
+    else if (btInput == "Z32091") {
+      String response = "?+" + String(L) + "m" + String(Y) + "+" + String(X) + "d+" + String(L) + "*00+00+00062\x03";
+      SerialBT.println(response);
+      Serial.print("Sent response: ");
+      Serial.println(response);
     }
-    else
-    {
-        Serial.print("Received invalid request: ");
-        Serial.println(request);
+    else if (btInput == "Z34093") {
+      String response = "(+" + String(L) + "0m" + String(Y) + "+" + String(X) + "d+" + String(L) + "t00+0000+000**070\x03";
+      SerialBT.println(response);
+      Serial.print("Sent response: ");
+      Serial.println(response);
     }
-}
-
-void loop()
-{
-    static String buffer = "";
-
-    // Read from Bluetooth
-    while (SerialBT.available())
-    {
-        char c = SerialBT.read();
-
-        if (c == START_CHAR)
-        {
-            buffer = ""; // Start new message
-        }
-        else if (c == END_CHAR)
-        {
-            processRequest(buffer);
-            buffer = "";
-        }
-        else
-        {
-            buffer += c;
-        }
+    else {
+      Serial.print("Unknown req: ");
+      Serial.println(btInput);
     }
-
-    // Optional: Add small delay to prevent watchdog triggers
-    delay(10);
+  }
+  
+  delay(10); // Small delay to prevent busy waiting
 }
